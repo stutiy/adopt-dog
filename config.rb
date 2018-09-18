cc5afb8cd8422a5c22d37aef1c03f73527c1f132
stuti <yogistuti@gmail.com>
2018-09-18 09:33:29 +0545

    navbar

---
+++ config.rb
@@ -0,0 +1,51 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+# http_path = "/"
+# css_dir = "stylesheets"
+# sass_dir = "sass"
+# images_dir = "images"
+# javascripts_dir = "javascripts"
+
+# General
+# output_style = :expanded
+# relative_assets = true
+# project_path = File.dirname(__FILE__) + '/'
+# env_file = project_path + 'environment.rb'
+# add_import_path project_path + '../all/src/assets/styles/src/'
+# lib_dir = project_path + '../../system/lib/'
+
+# Sass Paths
+http_path = '/src/assets/styles/sass'
+# http_javascripts_path = http_path + 'assets/scripts/'
+# http_stylesheets_path = http_path + 'assets/styles/'
+# http_images_path = http_stylesheets_path + 'images/'
+# http_fonts_path = http_stylesheets_path + 'fonts/'
+
+# Sass Directories
+javascripts_dir = '/assets/styles/js'
+css_dir = '/assets/styles/css'
+# css_dir = '/assets/styles/sass/layout'
+# sass_dir = css_dir + 'src/'
+sass_dir = '/assets/styles/sass'
+#images_dir = css_dir + 'images/'
+#fonts_dir = css_dir + 'fonts/'
+
+output_style = :expanded
+sass_options = { :debug_info => true }
+sourcemap = true
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
